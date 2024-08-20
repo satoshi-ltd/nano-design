ce127eb0922bc4bf602a9a0d7ec3eef8203cddd4
Mikel Garcia <mikelgarciaurbina@gmail.com>
2024-08-20 10:52:49 +0200

    Add Heading component

---
+++ src/components/Heading/Heading.style.js
@@ -0,0 +1,9 @@
+import StyleSheet from 'react-native-extended-stylesheet';
+
+export const style = StyleSheet.create({
+  heading: {
+    alignItems: 'center',
+    marginBottom: '$spaceXS',
+    marginHorizontal: '$spaceM',
+  },
+});
--- src/components/index.js
+++ src/components/index.js
@@ -2,6 +2,7 @@ export * from './Action';
 export * from './Button';
 export * from './Card';
 export * from './Confirm';
+export * from './Heading';
 export * from './Modal';
 export * from './Pagination';
 export * from './Screen';
