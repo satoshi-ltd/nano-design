8e6d4e85f9a5dfebaaa5ea6fd7b447c45764199b
Mikel Garcia <mikelgarciaurbina@gmail.com>
2024-08-21 10:10:25 +0200

    Requested changes from code review

--- src/components/SectionList/SectionList.jsx
+++ src/components/SectionList/SectionList.jsx
@@ -1,75 +1,28 @@
 import PropTypes from 'prop-types';
-import React, { useState } from 'react';
+import React from 'react';
 import { SectionList as BaseSectionList } from 'react-native';
 
-import { style } from './SectionList.style';
-import { Input } from '../../primitives';
-import { Action } from '../Action';
-import { Heading } from '../Heading';
-
-const SectionList = ({
-  closeLabel,
-  data,
-  keyExtractor,
-  querySearchData,
-  renderItem,
-  renderSectionHeader,
-  searchLabel,
-  title,
-}) => {
-  const [search, setSearch] = useState(false);
-  const [query, setQuery] = useState();
-  const [page, setPage] = useState(2);
-
-  const handleSearch = () => {
-    setSearch(() => {
-      setQuery(undefined);
-      return !search;
-    });
-  };
-
-  return (
-    <>
-      {data.length > 0 && (
-        <>
-          <Heading value={title}>
-            <Action caption color="content" onPress={handleSearch}>
-              {!search ? searchLabel || 'Search' : closeLabel || 'Close'}
-            </Action>
-          </Heading>
-          {search && (
-            <Input
-              autoFocus
-              placeholder={`${searchLabel || 'Search'}...`}
-              value={query}
-              onChange={setQuery}
-              style={style.inputSearch}
-            />
-          )}
-          <BaseSectionList
-            sections={querySearchData({ query, page }) || data}
-            keyExtractor={keyExtractor}
-            renderItem={renderItem}
-            renderSectionHeader={renderSectionHeader}
-            initialNumToRender={20}
-            onEndReached={() => setPage(page + 1)}
-            style={[style.sectionList]}
-          />
-        </>
-      )}
-    </>
-  );
-};
+const SectionList = ({ data, keyExtractor, onEndReached, refreshing, renderItem, renderSectionHeader, style }) => (
+  <BaseSectionList
+    initialNumToRender={20}
+    keyExtractor={keyExtractor}
+    onEndReached={onEndReached}
+    refreshing={refreshing}
+    renderItem={renderItem}
+    renderSectionHeader={renderSectionHeader}
+    sections={data}
+    style={style}
+  />
+);
 
 SectionList.propTypes = {
-  closeLabel: PropTypes.string,
   data: PropTypes.array,
   keyExtractor: PropTypes.func,
-  querySearchData: PropTypes.func,
+  onEndReached: PropTypes.func,
+  refreshing: PropTypes.bool,
   renderItem: PropTypes.func,
   renderSectionHeader: PropTypes.func,
-  searchLabel: PropTypes.string,
-  title: PropTypes.string,
+  style: PropTypes.any,
 };
 
 export { SectionList };
--- src/components/index.js
+++ src/components/index.js
@@ -2,7 +2,6 @@ export * from './Action';
 export * from './Button';
 export * from './Card';
 export * from './Confirm';
-export * from './Heading';
 export * from './Modal';
 export * from './Pagination';
 export * from './Screen';
