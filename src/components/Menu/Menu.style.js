6c6921917773621ec357ebb1d7067789916b564b
XΛVI <hello@soyjavi.com>
2024-09-29 13:37:53 +0700

    🆕 component <Menu>

---
+++ src/components/Menu/Menu.style.js
@@ -0,0 +1,12 @@
+import StyleSheet from 'react-native-extended-stylesheet';
+
+export const style = StyleSheet.create({
+  modal: {
+    paddingHorizontal: '$viewOffset * 2',
+    paddingTop: '$viewOffset',
+  },
+
+  critical: {
+    marginTop: '$viewOffset',
+  },
+});
