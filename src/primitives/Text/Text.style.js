6426b30142a787dcdd33650d433c465de4300253
soyjavi <hello@soyjavi.com>
2025-01-02 10:14:23 +0700

    📦 v0.2.41

--- src/primitives/Text/Text.style.js
+++ src/primitives/Text/Text.style.js
@@ -2,24 +2,24 @@ import StyleSheet from 'react-native-extended-stylesheet';
 
 export const style = StyleSheet.create({
   text: {
-    fontFamily: 'font-default',
+    fontFamily: '$font-default',
     fontWeight: '$fontWeightDefault',
     userSelect: 'none',
   },
 
   textSecondary: {
-    fontFamily: 'font-default-secondary',
+    fontFamily: '$font-default-secondary',
     fontWeight: '$fontWeightDefaultSecondary',
     userSelect: 'none',
   },
 
   bold: {
-    fontFamily: 'font-bold',
+    fontFamily: '$font-bold',
     fontWeight: '$fontWeightBold',
   },
 
   boldSecondary: {
-    fontFamily: 'font-bold-secondary',
+    fontFamily: '$font-bold-secondary',
     fontWeight: '$fontWeightBoldSecondary',
   },
 
