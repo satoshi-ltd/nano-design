96280e897b16e7bec790b41214c76172df96561b
soyjavi <hello@soyjavi.com>
2025-01-19 08:06:49 +0700

    Fix spacing in <Notification>

--- src/components/Notification/Notification.jsx
+++ src/components/Notification/Notification.jsx
@@ -7,11 +7,11 @@ import { style } from './Notification.style';
 import { Icon, Pressable, Text, View } from '../../primitives';
 
 const Notification = ({ children, error = false, icon, text, visible, onClose, ...others }) => {
-  const translateY = useRef(new Animated.Value(-100)).current; // Valor inicial fuera de la pantalla
+  const translateY = useRef(new Animated.Value(-128)).current;
 
   useEffect(() => {
     if (visible && Platform.OS === 'ios') Haptics.notificationAsync();
-    Animated.timing(translateY, { toValue: visible ? 0 : -100, duration: 300, useNativeDriver: true }).start();
+    Animated.timing(translateY, { toValue: visible ? 0 : -128, duration: 300, useNativeDriver: true }).start();
     // eslint-disable-next-line react-hooks/exhaustive-deps
   }, [visible]);
 
