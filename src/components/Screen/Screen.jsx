8c251615cdfc4f007db7686a8303fd0fe981a99a
Mikel Garcia <mikelgarciaurbina@gmail.com>
2025-01-24 12:23:03 +0100

    Protect the screen in background

--- src/components/Screen/Screen.jsx
+++ src/components/Screen/Screen.jsx
@@ -1,13 +1,15 @@
 import { useFocusEffect } from '@react-navigation/native';
 import { ScrollView, View } from '@satoshi-ltd/nano-design';
+import { BlurView } from 'expo-blur';
 import PropTypes from 'prop-types';
-import React, { useCallback, useRef } from 'react';
-import { KeyboardAvoidingView, Platform } from 'react-native';
+import React, { useCallback, useEffect, useRef, useState } from 'react';
+import { AppState, KeyboardAvoidingView, Modal, Platform, StyleSheet } from 'react-native';
 import { SafeAreaView } from 'react-native-safe-area-context';
 
 import { style } from './Screen.style';
 
 const Screen = ({ disableScroll = false, refreshScroll = true, ...others }) => {
+  const [inBackground, setInBackground] = useState(false);
   const scrollview = useRef(null);
 
   useFocusEffect(
@@ -18,24 +20,40 @@ const Screen = ({ disableScroll = false, refreshScroll = true, ...others }) => {
     }, []),
   );
 
+  useEffect(() => {
+    const handleAppStateChange = (state) => {
+      setInBackground(state === 'background' || state === 'inactive');
+    };
+
+    AppState.addEventListener('change', handleAppStateChange);
+    return () => {
+      AppState.removeEventListener('change', handleAppStateChange);
+    };
+  }, []);
+
   const isWeb = Platform.OS === 'web';
 
   return (
-    <SafeAreaView style={style.safeAreaView}>
-      {/* <TouchableWithoutFeedback onPress={Keyboard.dismiss} style={{ flex: 1 }}> */}
-      {React.createElement(
-        !isWeb ? KeyboardAvoidingView : React.Fragment,
-        !isWeb ? { behavior: disableScroll ? 'padding' : 'height' } : {},
-        !disableScroll ? (
-          <ScrollView ref={scrollview} style={style.scrollView}>
+    <>
+      <Modal visible={inBackground} transparent={true} animationType="fade">
+        <BlurView style={StyleSheet.absoluteFillObject} />
+      </Modal>
+      <SafeAreaView style={style.safeAreaView}>
+        {/* <TouchableWithoutFeedback onPress={Keyboard.dismiss} style={{ flex: 1 }}> */}
+        {React.createElement(
+          !isWeb ? KeyboardAvoidingView : React.Fragment,
+          !isWeb ? { behavior: disableScroll ? 'padding' : 'height' } : {},
+          !disableScroll ? (
+            <ScrollView ref={scrollview} style={style.scrollView}>
+              <View {...others} />
+            </ScrollView>
+          ) : (
             <View {...others} />
-          </ScrollView>
-        ) : (
-          <View {...others} />
-        ),
-      )}
-      {/* </TouchableWithoutFeedback> */}
-    </SafeAreaView>
+          ),
+        )}
+        {/* </TouchableWithoutFeedback> */}
+      </SafeAreaView>
+    </>
   );
 };
 
