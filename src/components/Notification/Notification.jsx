dfdfb9eb061bb1e7906a9d10589ff8a047f572bc
soyjavi <hello@soyjavi.com>
2025-02-09 07:39:03 +0700

    📦 v0.2.66

--- src/components/Notification/Notification.jsx
+++ src/components/Notification/Notification.jsx
@@ -1,20 +1,22 @@
 import * as Haptics from 'expo-haptics';
 import PropTypes from 'prop-types';
 import React, { useEffect, useRef } from 'react';
-import { Animated, Easing, Platform, SafeAreaView } from 'react-native';
+import { Animated, Easing, Platform, SafeAreaView, useWindowDimensions } from 'react-native';
 
 import { style } from './Notification.style';
 import { Icon, Pressable, Text, View } from '../../primitives';
 
 const Notification = ({ children, error = false, icon, text, title, visible, onClose, ...others }) => {
-  const translateY = useRef(new Animated.Value(-128)).current;
+  const { height } = useWindowDimensions();
+
+  const translateY = useRef(new Animated.Value(-height)).current;
 
   useEffect(() => {
     if (visible && Platform.OS === 'ios') Haptics.notificationAsync();
     Animated.timing(translateY, {
-      duration: 300,
+      duration: 400,
       easing: Easing.inOut(Easing.ease),
-      toValue: visible ? 0 : -128,
+      toValue: visible ? 0 : -height,
       useNativeDriver: true,
     }).start();
     // eslint-disable-next-line react-hooks/exhaustive-deps
