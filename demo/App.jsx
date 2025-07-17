e369af9d2b7055b8bb90b9fd1ad3246e8c560beb
soyjavi <hello@soyjavi.com>
2025-07-17 21:35:46 +0700

    New environment expo v53

--- demo/App.jsx
+++ demo/App.jsx
@@ -1,4 +1,5 @@
-import { Button, Card, Image, Text, View } from '@satoshi-ltd/nano-design';
+import { Card, Setting, View } from '@satoshi-ltd/nano-design';
+import { registerRootComponent } from 'expo';
 import React from 'react';
 import StyleSheet from 'react-native-extended-stylesheet';
 
@@ -6,52 +7,19 @@ import { CommonTheme } from './theme';
 
 StyleSheet.build(CommonTheme);
 
-const cardCommons = {
-  flex: true,
-  onPress: () => {
-    console.log('pressed');
-  },
-  children: (
-    <>
-      <Text>Hello</Text>
-      <Button>test</Button>
-    </>
-  ),
-};
-
-export default function App() {
+const App = () => {
   return (
     <>
-      <Image
-        source={{ uri: 'https://picsum.photos/300/200' }}
-        style={{ position: 'absolute', top: 0, left: 0, right: 0, bottom: 0 }}
-        resizeMode="cover"
-      />
       <View align="center" flex gap offset>
-        <Card {...cardCommons} />
-
-        <Card {...cardCommons} color="accent" />
-
-        <Card {...cardCommons} blur color="#ff00ff" blurOpacity={0.1} blurIntensity={50} />
-
-        <Card {...cardCommons} blur color="#fff000" blurOpacity={0.33} blurIntensity={33} />
-
-        <Card {...cardCommons} image={{ uri: 'https://picsum.photos/300/200' }} />
-        
-        <Card {...cardCommons} image={{ uri: 'https://picsum.photos/400/250' }} placeholderColor="#e0e0e0" />
-
-        <Image
-          source={{ uri: 'https://picsum.photos/300/200' }}
-          style={{ width: 300, height: 200, borderRadius: 8 }}
-          resizeMode="cover"
-          activityColor="gray"
-          activitySize="large"
-        />
-
-        <Button disabled large>
-          Test Component
-        </Button>
+        <Card>
+          <Setting activity caption="test" text="test" icon="home" />
+          <Setting caption="test" text="test" icon="home" />
+          <Setting caption="test" text="test" icon="home" />
+          <Setting caption="test" text="test" icon="home" />
+        </Card>
       </View>
     </>
   );
-}
+};
+
+registerRootComponent(App);
